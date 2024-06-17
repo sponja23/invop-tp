079620d4a97ae9f6448cb0d3a9e6700c0778da92
sponja23 <tomas.spognardi@gmail.com>
2024-06-16 23:26:04 -0300

    Enunciado y template

---
+++ asignacion_cuadrillas.py
@@ -0,0 +1,178 @@
+import sys
+#importamos el modulo cplex
+import cplex
+from recordclass import recordclass
+
+TOLERANCE =10e-6 
+Orden = recordclass('Orden', 'id beneficio cant_trab')
+
+class InstanciaAsignacionCuadrillas:
+    def __init__(self):
+        self.cantidad_trabajadores = 0
+        self.cantidad_ordenes = 0
+        self.ordenes = []
+        self.conflictos_trabajadores = []
+        self.ordenes_correlativas = []
+        self.ordenes_conflictivas = []
+        self.ordenes_repetitivas = []
+        
+    def leer_datos(self,nombre_archivo):
+
+        # Se abre el archivo
+        f = open(nombre_archivo)
+
+        # Lectura cantidad de trabajadores
+        self.cantidad_trabajadores = int(f.readline())
+        
+        # Lectura cantidad de ordenes
+        self.cantidad_ordenes = int(f.readline())
+        
+        # Lectura de las ordenes
+        self.ordenes = []
+        for i in range(self.cantidad_ordenes):
+            linea = f.readline().rstrip().split(' ')
+            self.ordenes.append(Orden(linea[0],linea[1],linea[2]))
+        
+        # Lectura cantidad de conflictos entre los trabajadores
+        cantidad_conflictos_trabajadores = int(f.readline())
+        
+        # Lectura conflictos entre los trabajadores
+        self.conflictos_trabajadores = []
+        for i in range(cantidad_conflictos_trabajadores):
+            linea = f.readline().split(' ')
+            self.conflictos_trabajadores.append(list(map(int,linea)))
+            
+        # Lectura cantidad de ordenes correlativas
+        cantidad_ordenes_correlativas = int(f.readline())
+        
+        # Lectura ordenes correlativas
+        self.ordenes_correlativas = []
+        for i in range(cantidad_ordenes_correlativas):
+            linea = f.readline().split(' ')
+            self.ordenes_correlativas.append(list(map(int,linea)))
+            
+        # Lectura cantidad de ordenes conflictivas
+        cantidad_ordenes_conflictivas = int(f.readline())
+        
+        # Lectura ordenes conflictivas
+        self.ordenes_conflictivas = []
+        for i in range(cantidad_ordenes_conflictivas):
+            linea = f.readline().split(' ')
+            self.ordenes_conflictivas.append(list(map(int,linea)))
+        
+        # Lectura cantidad de ordenes repetitivas
+        cantidad_ordenes_repetitivas = int(f.readline())
+        
+        # Lectura ordenes repetitivas
+        self.ordenes_repetitivas = []
+        for i in range(cantidad_ordenes_repetitivas):
+            linea = f.readline().split(' ')
+            self.ordenes_repetitivas.append(list(map(int,linea)))
+        
+        # Se cierra el archivo de entrada
+        f.close()
+
+
+def cargar_instancia():
+    # El 1er parametro es el nombre del archivo de entrada 	
+    nombre_archivo = sys.argv[1].strip()
+    # Crea la instancia vacia
+    instancia = InstanciaAsignacionCuadrillas()
+    # Llena la instancia con los datos del archivo de entrada 
+    instancia.leer_datos(nombre_archivo)
+    return instancia
+
+def agregar_variables(prob, instancia):
+    # Definir y agregar las variables:
+	# metodo 'add' de 'variables', con parametros:
+	# obj: costos de la funcion objetivo
+	# lb: cotas inferiores
+    # ub: cotas superiores
+    # types: tipo de las variables
+    # names: nombre (como van a aparecer en el archivo .lp)
+	
+    # Llenar coef\_funcion\_objetivo
+    coeficientes_funcion_objetivo = ....
+
+    # Poner nombre a las variables
+    nombres = ....
+    
+    # Agregar las variables
+    prob.variables.add(obj = coeficientes_funcion_objetivo, lb = ...., ub = ...., types=...., names=nombres)
+
+def agregar_restricciones(prob, instancia):
+    # Agregar las restricciones ax <= (>= ==) b:
+	# funcion 'add' de 'linear_constraints' con parametros:
+	# lin_expr: lista de listas de [ind,val] de a
+    # sense: lista de 'L', 'G' o 'E'
+    # rhs: lista de los b
+    # names: nombre (como van a aparecer en el archivo .lp)
+	
+    # Notar que cplex espera "una matriz de restricciones", es decir, una
+    # lista de restricciones del tipo ax <= b, [ax <= b]. Por lo tanto, aun cuando
+    # agreguemos una unica restriccion, tenemos que hacerlo como una lista de un unico
+    # elemento.
+
+    # Restriccion generica
+    indices = ...
+    valores = ...
+    fila = [indices,valores]
+    prob.linear_constraints.add(lin_expr=[fila], senses=[...], rhs=[...], names=[...])
+
+def armar_lp(prob, instancia):
+
+    # Agregar las variables
+    agregar_variables(prob, instancia)
+   
+    # Agregar las restricciones 
+    agregar_restricciones(prob, instancia)
+
+    # Setear el sentido del problema
+    prob.objective.set_sense(prob.objective.sense.....)
+
+    # Escribir el lp a archivo
+    prob.write('asignacionCuadrillas.lp')
+
+def resolver_lp(prob):
+    
+    # Definir los parametros del solver
+    prob.parameters....
+       
+    # Resolver el lp
+    prob.solve()
+
+#def mostrar_solucion(prob,instancia):
+    # Obtener informacion de la solucion a traves de 'solution'
+    
+    # Tomar el estado de la resolucion
+    status = prob.solution.get_status_string(status_code = prob.solution.get_status())
+    
+    # Tomar el valor del funcional
+    valor_obj = prob.solution.get_objective_value()
+    
+    print('Funcion objetivo: ',valor_obj,'(' + str(status) + ')')
+    
+    # Tomar los valores de las variables
+    x  = prob.solution.get_values()
+    # Mostrar las variables con valor positivo (mayor que una tolerancia)
+    .....
+
+def main():
+    
+    # Lectura de datos desde el archivo de entrada
+    instancia = cargar_instancia()
+    
+    # Definicion del problema de Cplex
+    prob = cplex.Cplex()
+    
+    # Definicion del modelo
+    armar_lp(prob,instancia)
+
+    # Resolucion del modelo
+    resolver_lp(prob)
+
+    # Obtencion de la solucion
+    mostrar_solucion(prob,instancia)
+
+if __name__ == '__main__':
+    main()
