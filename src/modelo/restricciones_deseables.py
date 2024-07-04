8d0c44a878c0c174c78635eaff796ea2d2a3ebd3
sponja23 <tomas.spognardi@gmail.com>
2024-07-03 23:06:27 -0300

    Refactor restricciones deseables

--- src/modelo/restricciones_deseables.py
+++ src/modelo/restricciones_deseables.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
+from collections.abc import Iterable
 from typing import TYPE_CHECKING
+
+from .objetivo import TerminosObjetivo
 
 from .variables import Variable
 
@@ -10,6 +13,198 @@ from ..instancia import InstanciaAsignacionCuadrillas
 
 if TYPE_CHECKING:
     from .modelo import ModeloAsignacionCuadrillas
+
+
+def restricciones_evitar_conflictos(
+    instancia: InstanciaAsignacionCuadrillas,
+) -> Iterable[Restriccion]:
+    """
+    Restricciones que evitan que dos trabajadores estén en el mismo lugar al mismo tiempo
+    """
+    return (
+        Restriccion(
+            terminos_izq=[(1, f"a_{i}_{j1}_{k}_{l}"), (1, f"a_{i}_{j2}_{k}_{l}")],
+            sentido="L",
+            term_independiente=1,
+        )
+        for j1, j2 in instancia.conflictos_trabajadores
+        for i in instancia.indices_ordenes
+        for k in instancia.indices_dias
+        for l in instancia.indices_turnos
+    )
+
+
+def variables_c_j1j2_i(
+    instancia: InstanciaAsignacionCuadrillas,
+) -> Iterable[Variable]:
+    """
+    Variables que indican si los trabajadores j1 y j2 están en conflicto en el orden i
+    """
+    return (
+        Variable(
+            nombre=f"c^{j1}_{j2}_{i}",
+            cota_inferior=0,
+            cota_superior=1,
+            tipo="B",
+        )
+        for j1, j2 in instancia.conflictos_trabajadores
+        for i in instancia.indices_ordenes
+    )
+
+
+def restricciones_definicion_c_j1j2_i(
+    instancia: InstanciaAsignacionCuadrillas,
+) -> Iterable[Restriccion]:
+    """
+    Restricciones que definen la variable c_{j1}_{j2}_{i}, que indica si los trabajadores j1 y j2 están en conflicto en el orden i
+    """
+    return (
+        Restriccion(
+            terminos_izq=[
+                (1, f"a_{i}_{j1}_{k}_{l}"),
+                (1, f"a_{i}_{j2}_{k}_{l}"),
+                (-1, f"c^{j1}_{j2}_{i}"),
+            ],
+            sentido="L",
+            term_independiente=1,
+        )
+        for j1, j2 in instancia.conflictos_trabajadores
+        for i in instancia.indices_ordenes
+        for k in instancia.indices_dias
+        for l in instancia.indices_turnos
+    )
+
+
+def objetivo_multa_conflictos(
+    instancia: InstanciaAsignacionCuadrillas,
+    penalizacion: float,
+) -> TerminosObjetivo:
+    """
+    Objetivo que penaliza la presencia de conflictos entre trabajadores
+    """
+    return (
+        (-penalizacion, f"c^{j1}_{j2}_{i}")
+        for j1, j2 in instancia.conflictos_trabajadores
+        for i in instancia.indices_ordenes
+    )
+
+
+def variables_t_ij(
+    instancia: InstanciaAsignacionCuadrillas,
+) -> Iterable[Variable]:
+    """
+    Variables que indican si el trabajador j realiza el orden i
+    """
+    return (
+        Variable(
+            nombre=f"t_{i}_{j}",
+            cota_inferior=0,
+            cota_superior=1,
+            tipo="B",
+        )
+        for i in instancia.indices_ordenes
+        for j in instancia.indices_trabajadores
+    )
+
+
+def restricciones_definicion_t_ij(
+    instancia: InstanciaAsignacionCuadrillas,
+) -> Iterable[Restriccion]:
+    """
+    Restricciones que definen la variable t_{i}_{j}, que indica si el trabajador j realiza el orden i
+    """
+    return (
+        Restriccion(
+            terminos_izq=[
+                (1, f"a_{i}_{j}_{k}_{l}")
+                for k in instancia.indices_dias
+                for l in instancia.indices_turnos
+            ],
+            sentido="E",
+            terminos_der=[(1, f"t_{i}_{j}")],
+        )
+        for i in instancia.indices_ordenes
+        for j in instancia.indices_trabajadores
+    )
+
+
+def restricciones_evitar_repeticiones(
+    instancia: InstanciaAsignacionCuadrillas,
+) -> Iterable[Restriccion]:
+    """
+    Restricciones que evitan que un trabajador realice dos órdenes consecutivas
+    """
+    return (
+        Restriccion(
+            terminos_izq=[(1, f"t_{i1}_{j}"), (1, f"t_{i2}_{j}")],
+            sentido="L",
+            term_independiente=1,
+        )
+        for i1, i2 in instancia.ordenes_repetitivas
+        for j in instancia.indices_trabajadores
+    )
+
+
+def variables_re_i1i2_j(
+    instancia: InstanciaAsignacionCuadrillas,
+) -> Iterable[Variable]:
+    """
+    Variables que indican si el trabajador j realiza los órdenes i e i+1
+    """
+    return (
+        Variable(
+            nombre=f"re_{i1}_{i2}_{j}",
+            cota_inferior=0,
+            cota_superior=1,
+            tipo="B",
+        )
+        for i1, i2 in instancia.ordenes_repetitivas
+        for j in instancia.indices_trabajadores
+    )
+
+
+def restricciones_definicion_re_i1i2_j(
+    instancia: InstanciaAsignacionCuadrillas,
+) -> Iterable[Restriccion]:
+    """
+    Restricciones que definen la variable re_{i1}_{i2}_{j}, que indica si el trabajador j realiza los órdenes i e i+1
+    """
+    return (
+        Restriccion(
+            terminos_izq=[
+                (1.0, f"a_{i}_{j}_{k}_{l}")
+                for i in (i1, i2)
+                for k in instancia.indices_dias
+                for l in instancia.indices_turnos
+            ]
+            + [(-1.0, f"re_{i1}_{i2}_{j}")],
+            sentido="L",
+            term_independiente=1,
+        )
+        for i1, i2 in instancia.ordenes_repetitivas
+        for j in instancia.indices_trabajadores
+    )
+
+
+def objetivo_multa_repeticiones(
+    instancia: InstanciaAsignacionCuadrillas,
+    penalizacion: float,
+) -> TerminosObjetivo:
+    """
+    Objetivo que penaliza la presencia de repeticiones de trabajadores
+    """
+    return (
+        (-penalizacion, f"re_{i1}_{i2}_{j}")
+        for i1, i2 in instancia.ordenes_repetitivas
+        for j in instancia.indices_trabajadores
+    )
+
+
+########################################################
+#                                                      #
+#  Estrategias para manejar conflictos y repeticiones  #
+#                                                      #
+########################################################
 
 
 class EstrategiaConflictos(ABC):
@@ -39,17 +234,7 @@ class EvitarConflictos(EstrategiaConflictos):
         instancia: InstanciaAsignacionCuadrillas,
         modelo: "ModeloAsignacionCuadrillas",
     ) -> None:
-        modelo.agregar_restricciones(
-            Restriccion(
-                terminos_izq=[(1, f"a_{i}_{j1}_{k}_{l}"), (1, f"a_{i}_{j2}_{k}_{l}")],
-                sentido="L",
-                term_independiente=1,
-            )
-            for j1, j2 in instancia.conflictos_trabajadores
-            for i in instancia.indices_ordenes
-            for k in instancia.indices_dias
-            for l in instancia.indices_turnos
-        )
+        modelo.agregar_restricciones(restricciones_evitar_conflictos(instancia))
 
 
 class MultarConflictos(EstrategiaConflictos):
@@ -64,38 +249,11 @@ class MultarConflictos(EstrategiaConflictos):
         instancia: InstanciaAsignacionCuadrillas,
         modelo: "ModeloAsignacionCuadrillas",
     ) -> None:
-        modelo.agregar_variables(
-            Variable(
-                nombre=f"c^{j1}_{j2}_{i}",
-                cota_inferior=0,
-                cota_superior=1,
-                tipo="B",
-            )
-            for j1, j2 in instancia.conflictos_trabajadores
-            for i in instancia.indices_ordenes
-        )
+        modelo.agregar_variables(variables_c_j1j2_i(instancia))
 
-        modelo.agregar_restricciones(
-            Restriccion(
-                terminos_izq=[
-                    (1.0, f"a_{i}_{j}_{k}_{l}")
-                    for j in (j1, j2)
-                    for k in instancia.indices_dias
-                    for l in instancia.indices_turnos
-                ]
-                + [(-1.0, f"c^{j1}_{j2}_{i}")],
-                sentido="L",
-                term_independiente=1,
-            )
-            for j1, j2 in instancia.conflictos_trabajadores
-            for i in instancia.indices_ordenes
-        )
+        modelo.agregar_restricciones(restricciones_definicion_c_j1j2_i(instancia))
 
-        modelo.agregar_objetivo(
-            (-self.penalizacion, f"c^{j1}_{j2}_{i}")
-            for j1, j2 in instancia.conflictos_trabajadores
-            for i in instancia.indices_ordenes
-        )
+        modelo.agregar_objetivo(objetivo_multa_conflictos(instancia, self.penalizacion))
 
 
 class EstrategiaRepeticiones(ABC):
@@ -114,30 +272,9 @@ class EstrategiaRepeticiones(ABC):
         instancia: InstanciaAsignacionCuadrillas,
         modelo: "ModeloAsignacionCuadrillas",
     ) -> None:
-        modelo.agregar_variables(
-            Variable(
-                nombre=f"t_{i}_{j}",
-                cota_inferior=0,
-                cota_superior=1,
-                tipo="B",
-            )
-            for i in instancia.indices_ordenes
-            for j in instancia.indices_trabajadores
-        )
+        modelo.agregar_variables(variables_t_ij(instancia))
 
-        modelo.agregar_restricciones(
-            Restriccion(
-                terminos_izq=[
-                    (1, f"a_{i}_{j}_{k}_{l}")
-                    for k in instancia.indices_dias
-                    for l in instancia.indices_turnos
-                ],
-                sentido="E",
-                terminos_der=[(1, f"t_{i}_{j}")],
-            )
-            for i in instancia.indices_ordenes
-            for j in instancia.indices_trabajadores
-        )
+        modelo.agregar_restricciones(restricciones_definicion_t_ij(instancia))
 
 
 class IgnorarRepeticiones(EstrategiaRepeticiones):
@@ -157,15 +294,7 @@ class EvitarRepeticiones(EstrategiaRepeticiones):
     ) -> None:
         self.agregar_variables_trabajador_orden(instancia, modelo)
 
-        modelo.agregar_restricciones(
-            Restriccion(
-                terminos_izq=[(1, f"t_{i1}_{j}"), (1, f"t_{i2}_{j}")],
-                sentido="L",
-                term_independiente=1,
-            )
-            for i1, i2 in instancia.ordenes_repetitivas
-            for j in instancia.indices_trabajadores
-        )
+        modelo.agregar_restricciones(restricciones_evitar_repeticiones(instancia))
 
 
 class MultarRepeticiones(EstrategiaRepeticiones):
@@ -182,35 +311,10 @@ class MultarRepeticiones(EstrategiaRepeticiones):
     ) -> None:
         self.agregar_variables_trabajador_orden(instancia, modelo)
 
-        modelo.agregar_variables(
-            Variable(
-                nombre=f"re^{i1}_{i2}_{j}",
-                cota_inferior=0,
-                cota_superior=1,
-                tipo="B",
-            )
-            for i1, i2 in instancia.ordenes_repetitivas
-            for j in instancia.indices_trabajadores
-        )
+        modelo.agregar_variables(variables_re_i1i2_j(instancia))
 
-        modelo.agregar_restricciones(
-            Restriccion(
-                terminos_izq=[
-                    (1.0, f"a_{i}_{j}_{k}_{l}")
-                    for i in (i1, i2)
-                    for k in instancia.indices_dias
-                    for l in instancia.indices_turnos
-                ]
-                + [(-1.0, f"re^{i1}_{i2}_{j}")],
-                sentido="L",
-                term_independiente=1,
-            )
-            for i1, i2 in instancia.ordenes_repetitivas
-            for j in instancia.indices_trabajadores
-        )
+        modelo.agregar_restricciones(restricciones_definicion_re_i1i2_j(instancia))
 
         modelo.agregar_objetivo(
-            (-self.penalizacion, f"re^{i1}_{i2}_{j}")
-            for i1, i2 in instancia.ordenes_repetitivas
-            for j in instancia.indices_trabajadores
+            objetivo_multa_repeticiones(instancia, self.penalizacion)
         )
