e33e87c9ada396df17236a2cb141e894bf4c5724
sponja23 <tomas.spognardi@gmail.com>
2024-07-08 20:50:51 -0300

    Agrego parámetros al solver

--- src/solver.py
+++ src/solver.py
@@ -7,6 +7,26 @@ import cplex
 TOL = 1e-10
 
 
+class SeleccionDeNodo(Enum):
+    """Estrategias para la selección de nodos en el árbol de branch-and-bound"""
+
+    DFS = 0
+    BEST_BOUND = 1
+    BEST_ESTIMATE = 2
+    BEST_ESTIMATE_ALT = 3
+
+
+class SeleccionDeVariable(Enum):
+    """Estrategias para la selección de variables de corte en el branch-and-bound"""
+
+    MIN_INFEAS = -1
+    AUTO = 0
+    MAX_INFEAS = 1
+    PSEUDOCOST = 2
+    STRONG = 3
+    PSEUDOCOST_REDUCED = 4
+
+
 class PlanosDeCorte(Enum):
     """Estrategias para la generación de planos de corte"""
 
@@ -20,7 +40,14 @@ class PlanosDeCorte(Enum):
 class ConfiguracionCPLEX:
     """Configuración para resolver CPlex"""
 
-    sin_output: bool = False
+    sin_output: bool = True
+
+    estrategia_seleccion_nodo: SeleccionDeNodo = SeleccionDeNodo.BEST_BOUND
+    estrategia_seleccion_variable: SeleccionDeVariable = SeleccionDeVariable.AUTO
+
+    preprocesamiento: bool = True
+
+    heuristic_effort: float = 1.0
 
     planos_de_corte: PlanosDeCorte = PlanosDeCorte.AUTO
     planos_de_corte_gomory: PlanosDeCorte = PlanosDeCorte.AUTO
@@ -44,6 +71,15 @@ class ConfiguracionCPLEX:
         cpx.parameters.mip.cuts.cliques.set(self.planos_de_corte_clique.value)
         cpx.parameters.mip.cuts.covers.set(self.planos_de_corte_cover.value)
         cpx.parameters.mip.cuts.disjunctive.set(self.planos_de_corte_disjunctive.value)
+
+        cpx.parameters.mip.strategy.nodeselect.set(self.estrategia_seleccion_nodo.value)
+        cpx.parameters.mip.strategy.variableselect.set(
+            self.estrategia_seleccion_variable.value
+        )
+
+        cpx.parameters.preprocessing.presolve.set(self.preprocesamiento)
+
+        cpx.parameters.mip.strategy.heuristiceffort.set(self.heuristic_effort)
 
 
 class Solver:
