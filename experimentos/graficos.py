d6678dadebba442aceadf5fdf74a029af4c0af8c
sponja23 <tomas.spognardi@gmail.com>
2024-07-08 22:06:30 -0300

    Función para graficar boxplots

--- experimentos/graficos.py
+++ experimentos/graficos.py
@@ -1,5 +1,5 @@
 from collections.abc import Iterable
-from typing import Tuple, Union
+from typing import List, Tuple, Union
 
 import matplotlib as mpl
 import pandas as pd
@@ -59,6 +59,32 @@ def histograma_hue(
     fig, ax = plt.subplots(1, 1, figsize=figsize)
 
     sns.histplot(df, x=x, hue=hue, bins=bins, ax=ax)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+
+    return fig
+
+
+def boxplot(
+    datos: List[Iterable[Union[float, int]]],
+    labels: Iterable[str],
+    *,
+    xlabel: str,
+    ylabel: str,
+    figsize: Tuple[int, int] = (10, 5),
+) -> mpl.figure.Figure:
+    fig, ax = plt.subplots(1, 1, figsize=figsize)
+
+    df = pd.DataFrame(
+        [
+            {"value": value, "label": label}
+            for values, label in zip(datos, labels)
+            for value in values
+        ]
+    )
+
+    sns.boxplot(data=df, x="label", y="value", ax=ax)
+
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
 
